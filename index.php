a04965c0934240e2e7753fa3466f5940c318c772
nejcrogelsek <nejc.rogelsek40@gmail.com>
2017-11-10 21:42:11 +0100

    komentar

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>VR Trener</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td colspan="2"><h1>VR Trener</h1></td>
+			</tr>
+			<tr>
+				<td id="td_fitnes">Fitnes:</td>
+				<td id="td_izberi">
+					<select name="izberi" size="1">
+						<option value="velenje" selected="selected">Velenje</option>
+						<option value="slovenj_gradec">Slovenj Gradec</option>
+						<option value="ravne_na_koroškem">Ravne na Koroškem</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td id="td_vr_logo" colspan="2">
+					<a href="vr/index.php"><img src="logo.png" alt="vr_logo" height="80px" width="100px"></a> 
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
