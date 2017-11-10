0c3e14bf220f6e16d7d1a38e4ec4d35175aa89f6
BuildTools <unconfigured@null.spigotmc.org>
2017-11-10 17:59:43 +0100

    php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello, World! - A-Frame</title>
+    <meta name="description" content="Hello, World! - A-Frame">
+    <script src="aframe.js"></script>
+  </head>
+  <body>
+    <a-scene>
+      <a-sky id="img-sky" src="SAM_100_0090.jpg"></a-sky>
+	  <a-cylinder id="cyl" position="0 0 0"></a-cylinder>
+    </a-scene>
+  </body>
+</html>
