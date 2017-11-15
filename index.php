d8f0e6ae5afaca2538c0966c982ede9347edd30a
nejcrogelsek <nejc.rogelsek40@gmail.com>
2017-11-15 10:36:31 +0100

    slideShow slike

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 	<head>
 		<title>VR Trener</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="/scripts/slide.js"></script>
 	</head>
 	<body>
+		<div class="SlideShow" style="max-width:500px">
+			<img class="mySlides" src="nature1.jpg" width="100%">
+			<img class="mySlides" src="nature2.jpg" width="100%">
+			<img class="mySlides" src="nature3.jpg" width="100%">
+		</div>
 		<table>
+		<script>SlideShow()</script>
+			</tr>
 			<tr>
 				<td colspan="2"><img src="slike_prva_stran/logo.png" alt="logo" width="225" height="200"></td>
 			</tr>
@@ -29,8 +37,8 @@
 			</tr>
 			<tr>
 				<td id="td_vr_logo" colspan="2">
-					<!-- <a href="https://vrtrener-test.herokuapp.com/vr/index.php"><img src="slike_prva_stran/vr_logo.png" alt="vr_logo" height="80px" width="100px"></a> -->
-					<button class="button_vhod"><a href="https://vrtrener-test.herokuapp.com/vr/index.php">Vhod</a></button>
+					<!-- <a href="vr/index.php"><img src="slike_prva_stran/vr_logo.png" alt="vr_logo" height="80px" width="100px"></a> -->
+					<button class="button_vhod"><a href="vr/index.php">Vhod</a></button>
 				</td>
 			</tr>
 		</table>
