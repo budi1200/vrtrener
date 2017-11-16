8d41ca22bdb57df33f87d1cb679fc2837769fc59
nejcrogelsek <nejc.rogelsek40@gmail.com>
2017-11-16 18:39:17 +0100

    SlideShow

--- index.php
+++ index.php
@@ -3,51 +3,47 @@
 	<head>
 		<title>VR Trener</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<script src="/scripts/slide.js"></script>
+		<script src="scripts/slide.js"></script>
 	</head>
 	<body>
-		<div class="SlideShow" style="max-width:500px">
-			<img class="mySlides" src="nature1.jpg" width="100%">
-			<img class="mySlides" src="nature2.jpg" width="100%">
-			<img class="mySlides" src="nature3.jpg" width="100%">
-		</div>
-		<table>
-		<script>SlideShow()</script>
-			</tr>
-			<tr>
-				<td colspan="2"><img src="slike_prva_stran/logo.png" alt="logo" width="225" height="200"></td>
-			</tr>
-			<tr>
-				<td colspan="2">
-					<blockquote id="quote">"Take care of your body. It's the only place you have to live."</blockquote>
-				</td>
-			</tr>
-			<tr>
-				<td id="quote_author" colspan="2">Jim Rohn</td>
-			</tr>
-			<tr>
-				<td id="td_fitnes">Fitnes:</td>
-				<td id="td_izberi">
+		<div class="parent">
+			<div class="child">
+				<img src="slike_prva_stran/logo.png" alt="logo" width="225" height="200">
+			</div>
+			<div class="child">
+				<blockquote id="quote">"Take care of your body. It's the only place you have to live."</blockquote>
+			</div>
+			<div class="child">
+				<p id="quote_author">Jim Rohn</p>
+			</div>
+			<div class="child">
+				<p id="tipka_fitnes">Fitnes:
 					<select name="izberi" size="1">
 						<option value="velenje" selected="selected">Velenje</option>
 						<option value="slovenj_gradec">Slovenj Gradec</option>
 						<option value="ravne_na_koroškem">Ravne na Koroškem</option>
 					</select>
-				</td>
-			</tr>
-			<tr>
-				<td id="td_vr_logo" colspan="2">
-					<!-- <a href="vr/index.php"><img src="slike_prva_stran/vr_logo.png" alt="vr_logo" height="80px" width="100px"></a> -->
-					<button class="button_vhod"><a href="vr/index.php">Vhod</a></button>
-				</td>
-			</tr>
-		</table>
-		<footer>
-			<img class="image" src="slike_prva_stran/facebook.png" alt="facebook_icon" width="30" height="30">
-			<img class="image" src="slike_prva_stran/twitter.png" alt="twitter_icon" width="30" height="30">
-			<img class="image" src="slike_prva_stran/instagram.png" alt="instagram_icon" width="30" height="30">
-			<img class="image" src="slike_prva_stran/email.png" alt="email_icon" width="30" height="30">
-			<img class="image" src="slike_prva_stran/youtube.png" alt="youtube_icon" width="30" height="30">
-		</footer>
+				</p>
+			</div>
+			<div id="button">
+				<button class="button_vhod"><a href="vr/index.php">Vhod</a></button>
+			</div>
+			<div class="footer">
+				<img class="image" src="slike_prva_stran/facebook.png" alt="facebook_icon" width="30" height="30">
+				<img class="image" src="slike_prva_stran/twitter.png" alt="twitter_icon" width="30" height="30">
+				<img class="image" src="slike_prva_stran/instagram.png" alt="instagram_icon" width="30" height="30">
+				<img class="image" src="slike_prva_stran/email.png" alt="email_icon" width="30" height="30">
+				<img class="image" src="slike_prva_stran/youtube.png" alt="youtube_icon" width="30" height="30">
+			</div>
+		</div>
+        <div class="slide">
+			<img class="mySlides" src="slike_postopek_dela/fitnes_gim_zunaj/fitnes_gim_1.jpg" width="100%" height="100%">
+			<img class="mySlides" src="slike_postopek_dela/fitnes_gim_zunaj/fitnes_gim_2.jpg" width="100%" height="100%">
+			<img class="mySlides" src="slike_postopek_dela/fitnes_gim_zunaj/fitnes_gim_3.jpg" width="100%" height="100%">
+			<img class="mySlides" src="slike_postopek_dela/fitnes_gim_zunaj/fitnes_gim_4.jpg" width="100%" height="100%">
+			<img class="mySlides" src="slike_postopek_dela/fitnes_gim_zunaj/fitnes_gim_5.jpg" width="100%" height="100%">
+			<img class="mySlides" src="slike_postopek_dela/fitnes_gim_zunaj/fitnes_gim_6.jpg" width="100%" height="100%">
+		</div>
+		<script>SlideShow()</script>
 	</body>
 </html>
