d6477f849ce91d8f34e27d6f9d6fcdc985ef893f
BuildTools <alenab22@gmail.com>
2017-11-18 18:26:45 +0100

    animacija ob prikazu okna z vajo

--- scripts/custom.js
+++ scripts/custom.js
@@ -6,11 +6,15 @@ $(document).mousemove(function(e) {
 */
 function setWindow(el_id){
 	var test = document.getElementById(el_id);
-	test.style.display = "block";
+	//test.style.display = "block";
+    test.style.opacity = '0.9';
 	test.style.top = "20.5%";
 	test.style.left = "50.5%";
+    test.style.pointerEvents = 'auto';
 }
 
 function off(el_id){
-	document.getElementById(el_id).style.display = "none";
+	var a = document.getElementById(el_id);
+    a.style.opacity= '0';
+    a.style.pointerEvents = 'none';
 }
