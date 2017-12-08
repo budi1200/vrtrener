309cdefc1eeee3ea554f2d0f49f177f07845ff10
budi1200 <alenab22@gmail.com>
2017-12-08 21:18:30 +0100

    connect php5

--- connect.php
+++ connect.php
@@ -1,4 +1,9 @@
 <?php
-$dbopts = parse_url(getenv([DATABASE_URL]));
+$dbopts = parse_url(getenv('DATABASE_URL'));
 $db = pg_connect('host='.$dbopts["host"] . ' dbname='.ltrim($dbopts["path"],'/') . ' port='.$dbopts["port"] . ' user='.$dbopts["user"] . ' sslmode=require'. ' password='.$dbopts["pass"]);
+    if(db) {
+       echo 'connected';
+    } else {
+        echo 'there has been an error connecting';
+    }
 ?>
