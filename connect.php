76f14cdce3fa26506ddd1b64a228ff9d6acba28d
budi1200 <alenab22@gmail.com>
2017-12-08 21:10:21 +0100

    connect php3

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$db = pg_connect('host='.$dbopts["host"] . ' dbname='.ltrim($dbopts["path"],'/') . 'port='.$dbopts["port"] . ' user='.$dbopts["user"] ' sslmode=require'. ' password='.$dbopts["pass"]);
+$db = pg_connect('host='.$dbopts["host"] . ' dbname='.ltrim($dbopts["path"],'/') . 'port='.$dbopts["port"] . ' user='.$dbopts["user"] . ' sslmode=require'. ' password='.$dbopts["pass"]);
 ?>
