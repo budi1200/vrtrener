8ded09994da52f5771f4338a40d7b44ff32625d0
budi1200 <alenab22@gmail.com>
2017-12-18 11:42:11 +0100

    slideshow max width/height

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,14 +1,13 @@
 $(function() {
     //configuration
-    var width = $( window ).width();
+    var width = $(document).width();
     var animationSpeed = 1500;
-    var pause = 3000;
+    var pause = 5000;
     var currentSlide = 1;
-
     var $slider = $('#slider');
     var $slideContainer = $slider.find('.slides');
     var $slides = $slideContainer.find('.slide');
-
+    $('.slide').css("width", width);
     setInterval(function() {
         $slideContainer.animate({'margin-left': '-=' +width}, animationSpeed, function() {
             currentSlide++;
