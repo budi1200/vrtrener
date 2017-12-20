71e1b080acf3bf0f781195e6289ebc22f39ff339
BuildTools <alenab22@gmail.com>
2017-12-20 23:03:14 +0100

    scripts -> js folder

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="scripts/animate.js"></script>
+    <script type="text/javascript" src="js/animate.js"></script>
+    <script type="text/javascript" src="js/dynamicpage.js"
     <meta charset="utf-8">
 </head>
 
