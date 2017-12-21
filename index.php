ffc11d66b08f96698c46e5f34db64f1a76ecdb2c
nejcrogelsek <nejc.rogelsek40@gmail.com>
2017-12-21 13:49:43 +0100

    kom

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <head>
     <title>VR Trener</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <!--<link rel="stylesheet" type="text/css" href="animate.css">-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
