3c058f26e2b412bb8154efa24290d7b26f902da0
budi1200 <alenab22@gmail.com>
2017-12-21 07:47:27 +0100

    remove useless css file

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title>VR Trener</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" type="text/css" href="animate.css">
+    <!--<link rel="stylesheet" type="text/css" href="animate.css">-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
