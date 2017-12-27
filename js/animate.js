8a37938590e5a3c87c0e545bd828388146d183d4
budi1200 <alenab22@gmail.com>
2017-12-27 21:24:10 +0100

    dinamicno prilagajanje slideshowa ob spremembi velikosti okna

--- js/animate.js
+++ js/animate.js
@@ -1,13 +1,37 @@
+//del resize funkcije
+var waitForFinalEvent = (function () {
+  var timers = {};
+  return function (callback, ms, uniqueId) {
+    if (!uniqueId) {
+      uniqueId = "Don't call this twice without a uniqueId";
+    }
+    if (timers[uniqueId]) {
+      clearTimeout (timers[uniqueId]);
+    }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
+})();
+
+var pause = 10000;
 $(function() {
     //configuration
-    var width = $(document).width();
+    var width = $(document).width(); //width okna
     var animationSpeed = 1500;
-    var pause = 5000;
     var currentSlide = 1;
     var $slider = $('#slider');
     var $slideContainer = $slider.find('.slides');
     var $slides = $slideContainer.find('.slide');
     $('.slide').css("width", width);
+
+    //preveri ce je okno bilo razsirjeno
+    $(window).resize(function () {
+    waitForFinalEvent(function(){
+        width = $(document).width(); // spremeni width na nov width okna
+        $('.slide').css("width", width); //prilagodi width slik
+        $slideContainer.css('margin-left', currentSlide*width*(-1)); //prilagodi margin slideshova
+    }, 500);
+});
+
     setInterval(function() {
         $slideContainer.animate({'margin-left': '-=' +width}, animationSpeed, function() {
             currentSlide++;
