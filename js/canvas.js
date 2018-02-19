6323f827c0099b49e7ac7c973c20c7b9c4627592
budi1200 <alenab22@gmail.com>
2018-02-19 12:59:30 +0100

    Cleanup

--- js/canvas.js
+++ js/canvas.js
@@ -1,37 +1,12 @@
 function canv(){
-/*var video = document.getElementById('vid');
-    //console.log(video);
-var opis = document.getElementById("opis").innerHTML;
-    //console.log(opis);
-var naslov = document.getElementById("naslov").innerHTML;
-    //console.log(naslov);
-var text_x = 326;
-var text_y = 61;
-var text_maxWidth = 164;
-var lineHeight = 17;
-canv.fillStyle = "#CCC";
-canv.fillRect(0, 0, 500, 500);
-canv.fillStyle = "#000";
-canv.font = "bold 16px Arial";
-canv.fillText(naslov, 10, 26);
-canv.font = "14px Arial";*/
 var video = $('#vid').attr('url_slika');
 var frame = $('#vid').attr('frames_slika');
-/*video.addEventListener('play', function () {
-    var $this = this; //cache
-    (function loop() {
-        if (!$this.paused && !$this.ended) {
-            canv.drawImage($this, 20, 45, 300, 400);
-            setTimeout(loop, 1000 / 30); // drawing at 30fps
-        }
-    })();
-}, 0);*/
 
 var canvidControl = canvid({
     selector : '#canvWrapper',
     videos: {
         clip1: { src: video, frames: frame, cols: 6, fps: 6, loops: 1, onEnd: function(){
-          console.log('clip1 ended.');
+          //console.log('clip1 ended.');
           canvidControl.play('clip1');
         }},
     },
@@ -75,11 +50,8 @@ var canvidControl = canvid({
             }
             canv.fillText(line, x, y);
         }
-
         wrapText(canv, opis, text_x, text_y, text_maxWidth, lineHeight);
-
         $("#plan").attr('src', '#canvid');
     }
 });
-
 }
